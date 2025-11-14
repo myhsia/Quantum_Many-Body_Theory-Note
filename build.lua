c13936434bb465810eaf275a82c0bf0a9ae745f5
myhsia <myhsia@outlook.com>
2025-11-14 13:23:59 +0800

    Config l3build script build.lua.

---
+++ build.lua
@@ -0,0 +1,50 @@
+--[==========================================[--
+          L3BUILD FILE FOR [PHY5047]QMB
+     Check PDF File & Directory After Build
+--]==========================================]--
+
+--[==========================================[--
+                Basic Information
+             Do Check Before Upload
+--]==========================================]--
+module           = "QMB"
+version          = "2025-11-16"
+
+--[==========================================[--
+               Build, Pack Release
+         Do not Modify Unless Necessary
+--]==========================================]--
+ctanzip          = module
+excludefiles     = {"*~"}
+suppdirs         = {"chapter", "media"}
+unpacksuppfiles  = {"*.bib"}
+textfiles        = {"*.md", "LICENSE", "*.lua", "makefile", "*.bat"}
+typesetexe       = "latexmk"
+typesetfiles     = {module .. ".tex"}
+typesetopts      = "-pdf -interaction=nonstopmode"
+
+function docinit_hook()
+  for _,supp in pairs(suppdirs) do
+    run(currentdir, "cp -r " .. supp .. " " .. typesetdir)
+  end   
+  return 0
+end
+function tex(file,dir,cmd)
+  dir = dir or "."
+  cmd = cmd or typesetexe .. " " .. typesetopts
+  return run(dir, cmd .. file)
+end
+-- function copyctan()
+--   local pkgdir = ctandir .. "/" .. ctanpkg
+--   mkdir(pkgdir)
+--   for _,main in ipairs({typesetsuppfiles, pdffiles}) do
+--     for _,glob in pairs(main) do
+--       cp(glob, typesetdir, pkgdir)
+--     end
+--   end
+--   local pkgsuppdir = ctandir .. "/" .. ctanpkg .. "/" .. supportdir
+--   mkdir(pkgsuppdir)
+--   for _,supptab in pairs(typesetsuppfiles) do
+--     cp(supptab, supportdir, pkgsuppdir)
+--   end
+-- end
